2a18635b8561baa837228913c775e823bc230a5d
morganb75 <morgan.bleunven@maif.fr>
2024-05-25 00:44:36 +0200

    TP en cours

--- TP_liste_produits/src/App.jsx
+++ TP_liste_produits/src/App.jsx
@@ -1,5 +1,9 @@
 
 import './App.css'
+import { Checkbox } from './components/forms/Checkbox'
+import { Input } from './components/forms/Input'
+import { ProductRow } from './components/products/ProductRow'
+import { ProductCategoryRow } from './components/products/ProductCategoryRow'
 
 const PRODUCTS = [
   {category: "Fruits", price: "$1", stocked:true,name: "Apple"},
@@ -12,7 +16,46 @@ const PRODUCTS = [
 
 
 function App() {
+  return <div className='container my-3'>
+    <SearchBar/>
+    <ProductTable products={PRODUCTS}/>
+  </div>
+}
+
+function SearchBar() {
+  return <div>
+      <div className='mb-3'>
+          <Input value='' onChange={() =>null} placeholder='rechercher...' />
+          <Checkbox id="stocked" checked={false} onChange={() =>null} label="N'afficher que les produits en stock"/>
+      </div>
+  </div>
+}
+
+function ProductTable({products}){
   
+  const rows = []
+  let lastCategory = null
+
+  for (let product of products) {
+    if (product.category !== lastCategory){
+      rows.push(<ProductCategoryRow key={product.category} name={product.category}/>)
+    }
+    lastCategory = product.category
+    rows.push(<ProductRow key={product.name} product={product}/>)
+    
+  }
+
+  return <table className='table'>
+    <thead>
+      <tr>
+        <th>Nom</th>
+        <th>Prix</th>
+      </tr>
+    </thead>
+    <tbody>
+    {rows}
+    </tbody>
+  </table>
 }
 
 export default App
--- TP_liste_produits/src/components/forms/Checkbox.jsx
+++ TP_liste_produits/src/components/forms/Checkbox.jsx
@@ -1,10 +1,11 @@
-export function Checkbox({checked, onChange, label}) {
-    return <div>
-        <input type="checkbox" 
+export function Checkbox({checked, onChange, label, id}) {
+    return <div className="form-check">
+        <input  id={id}
+                type="checkbox" 
                 className="form-check-input"
                 checked={checked}
                 onChange={(e) => onChange(e.target.checked)}
         />
-        <label className="form-check-label">{label}</label>
+        <label htmlFor={id} className="form-check-label">{label}</label>
     </div>
 }
